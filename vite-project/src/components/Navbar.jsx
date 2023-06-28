b8b14fededcbb2c289a1fc4a6023406888b1f367
AlvinMuiru <alvinamuiru2022@gmail.com>
2023-06-28 21:09:55 +0300

    Creating an Airbnb experiences clone using React

---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import airbnblogo from "./images/airbnb-logo.png"
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={airbnblogo} className="nav--logo" />
+        </nav>
+    )
+}
